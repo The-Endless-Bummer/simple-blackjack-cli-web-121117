bb50be53cfac38923874faf6ede60942b7cb02d4
Maurice Roy <mautayro@gmail.com>
2017-11-28 22:58:27 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,10 +30,9 @@ end
 
 def hit?(card_total)
   prompt_user
-  get_user_input
-  if input == h
+  if get_user_input == h
     card_total += deal_card
-  elsif input != s
+  elsif get_user_input != s
     invalid_command
     prompt_user
   end
